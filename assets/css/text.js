402f3deba59fd4171e0002006eaa7a5143f4915e
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-07-02 14:50:56 +0800

    update

--- assets/css/text.js
+++
@@ -1,8 +0,0 @@
-function tampilkanTeksLain() {
-  var teksLain = document.getElementById("teks-blog");
-  if (teksLain.style.display === "none") {
-    teksLain.style.display = "block";
-  } else {
-    teksLain.style.display = "none";
-  }
-}
